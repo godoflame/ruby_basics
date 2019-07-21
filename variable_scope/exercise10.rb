ecf9028678780b45882f5b96407524a167507e49
Kyle Packard <godoflame@hotmail.com>
2019-07-21 16:50:46 -0500

    Add exercise 10 from variable scope section.

---
+++ variable_scope/exercise10.rb
@@ -0,0 +1,14 @@
+# Program throws an error, since a is not defined and there is no + method for
+# nil
+
+a = 7
+array = [1, 2, 3]
+
+def my_value(ary)
+  ary.each do |b|
+    a += b
+  end
+end
+
+my_value(array)
+puts a
