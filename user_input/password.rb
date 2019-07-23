fe06aeaeec44241f26f9c87dbdcfb25e75ede523
Kyle Packard <godoflame@hotmail.com>
2019-07-23 15:54:53 -0500

    Add exercise 6 from user input section.

---
+++ user_input/password.rb
@@ -0,0 +1,9 @@
+PASSWORD = 'SecreT'
+answer = nil
+loop do
+  puts ">> Please enter your password:"
+  answer = gets.chomp
+  break if answer == PASSWORD
+  puts ">> Invalid password!"
+end
+puts "Welcome!"
