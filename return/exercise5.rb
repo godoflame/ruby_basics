8020e2dda7111f3958e6c63a777949e9ff2ab524
Kyle Packard <godoflame@hotmail.com>
2019-07-23 19:47:33 -0500

    Add exercise 5 from return section.

---
+++ return/exercise5.rb
@@ -0,0 +1,7 @@
+# Code prints:
+#   Dinner
+#   nil
+
+# Since there's no explicit return call made in the meal method, the method
+# returns the value of the last line, which the puts 'Dinner' call value is
+# nil.
