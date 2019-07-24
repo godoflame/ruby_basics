afcaf51b2e283752269ac146e014236d5e85be3b
Kyle Packard <godoflame@hotmail.com>
2019-07-23 19:17:27 -0500

    Add exercise 9 from methods section.

---
+++ methods/exercise9.rb
@@ -0,0 +1,11 @@
+def add(first_number, second_number)
+  first_number + second_number
+end
+
+def multiply(first_number, second_number)
+  first_number * second_number
+end
+
+puts add(2,2) == 4
+puts add(5,4) == 9
+puts multiply(add(2,2), add(5,4)) == 36
