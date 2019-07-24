14ea3c0d653a5cc9fb72c45d9e00945bfd32d26d
Kyle Packard <godoflame@hotmail.com>
2019-07-23 19:53:37 -0500

    Add exercise 7 from return section.

---
+++ return/exercise7.rb
@@ -0,0 +1,11 @@
+# Will print:
+#  0
+#  1
+#  2
+#  3
+#  4
+#  5
+
+# The loop variable prints the current index of the loop, which will print 0-4
+# with 5 loops, and the method returns 5, since that's what's returned by the
+# times loop.
