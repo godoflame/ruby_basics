e19c374ac91b4a10019aed6aa047b2ef9efd04f3
Kyle Packard <godoflame@hotmail.com>
2019-07-25 19:01:33 -0500

    Add debugging folder and exercise 1 from debugging section.

---
+++ debugging/exercise1.rb
@@ -0,0 +1,6 @@
+# Will throw an error for incorrect number of arguments since the method is
+# expecting one array argument, not 6 individual arguments.  This can be fixed
+# by putting the arguments in an array.
+
+# The 2nd line will work as is, but without the array it'll end up throwing an
+# undefined method error since there's no integer class method each.
